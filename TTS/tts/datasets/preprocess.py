14f068db281c45e868bfe0d795ca81d9b7bc4d17
atlisig <atlisigurgeirsson@gmail.com>
2020-05-26 16:45:18 +0000

    added a LOBE normalizer to datasets/preprocess.py

--- TTS/tts/datasets/preprocess.py
+++ TTS/tts/datasets/preprocess.py
@@ -2,60 +2,27 @@ import os
 from glob import glob
 import re
 import sys
-from pathlib import Path
+from TTS.utils.generic_utils import split_dataset
 
-from tqdm import tqdm
 
-from TTS.tts.utils.generic_utils import split_dataset
-
-####################
-# UTILITIES
-####################
-
-def load_meta_data(datasets, eval_split=True):
+def load_meta_data(datasets):
     meta_data_train_all = []
-    meta_data_eval_all = [] if eval_split else None
+    meta_data_eval_all = []
     for dataset in datasets:
         name = dataset['name']
         root_path = dataset['path']
         meta_file_train = dataset['meta_file_train']
         meta_file_val = dataset['meta_file_val']
-        # setup the right data processor
         preprocessor = get_preprocessor_by_name(name)
-        # load train set
+
         meta_data_train = preprocessor(root_path, meta_file_train)
-        print(f" | > Found {len(meta_data_train)} files in {Path(root_path).resolve()}")
-        # load evaluation split if set
-        if eval_split:
-            if meta_file_val is None:
-                meta_data_eval, meta_data_train = split_dataset(meta_data_train)
-            else:
-                meta_data_eval = preprocessor(root_path, meta_file_val)
-            meta_data_eval_all += meta_data_eval
+        if meta_file_val is None:
+            meta_data_eval, meta_data_train = split_dataset(meta_data_train)
+        else:
+            meta_data_eval = preprocessor(root_path, meta_file_val)
         meta_data_train_all += meta_data_train
-        # load attention masks for duration predictor training
-        if 'meta_file_attn_mask' in dataset:
-            meta_data = dict(load_attention_mask_meta_data(dataset['meta_file_attn_mask']))
-            for idx, ins in enumerate(meta_data_train_all):
-                attn_file = meta_data[ins[1]].strip()
-                meta_data_train_all[idx].append(attn_file)
-            if meta_data_eval_all is not None:
-                for idx, ins in enumerate(meta_data_eval_all):
-                    attn_file = meta_data[ins[1]].strip()
-                    meta_data_eval_all[idx].append(attn_file)
+        meta_data_eval_all += meta_data_eval
     return meta_data_train_all, meta_data_eval_all
-
-
-def load_attention_mask_meta_data(metafile_path):
-    """Load meta data file created by compute_attention_masks.py"""
-    with open(metafile_path, 'r') as f:
-        lines = f.readlines()
-
-    meta_data = []
-    for line in lines:
-        wav_file, attn_file = line.split('|')
-        meta_data.append([wav_file, attn_file])
-    return meta_data
 
 
 def get_preprocessor_by_name(name):
@@ -64,9 +31,21 @@ def get_preprocessor_by_name(name):
     return getattr(thismodule, name.lower())
 
 
-########################
-# DATASETS
-########################
+def lobe(root_path, meta_file):
+    """Normalize LOBE dataset, use info.json as meta_file
+    """
+    import json
+    info = json.load(open(os.path.join(root_path, meta_file)))
+    items = []
+    for rec_id, item in info.items():
+        speaker_id = item['collection_info']['user_id']
+        speaker_name = f"s_{speaker_id}"
+        recording_fname = item['recording_info']['recording_fname']
+        wav_file = os.path.join(root_path, 'audio', str(speaker_id), recording_fname)
+        text = item['text_info']['text']
+        items.append([text, wav_file, speaker_name])
+    return items
+
 
 def tweb(root_path, meta_file):
     """Normalize TWEB dataset.
@@ -82,6 +61,19 @@ def tweb(root_path, meta_file):
             text = cols[1]
             items.append([text, wav_file, speaker_name])
     return items
+
+
+# def kusal(root_path, meta_file):
+#     txt_file = os.path.join(root_path, meta_file)
+#     texts = []
+#     wavs = []
+#     with open(txt_file, "r", encoding="utf8") as f:
+#         frames = [
+#             line.split('\t') for line in f
+#             if line.split('\t')[0] in self.wav_files_dict.keys()
+#         ]
+#     # TODO: code the rest
+#     return  {'text': texts, 'wavs': wavs}
 
 
 def mozilla(root_path, meta_file):
@@ -117,10 +109,9 @@ def mozilla_de(root_path, meta_file):
 
 def mailabs(root_path, meta_files=None):
     """Normalizes M-AI-Labs meta data files to TTS format"""
-    speaker_regex = re.compile(
-        "by_book/(male|female)/(?P<speaker_name>[^/]+)/")
+    speaker_regex = re.compile("by_book/(male|female)/(?P<speaker_name>[^/]+)/")
     if meta_files is None:
-        csv_files = glob(root_path + "/**/metadata.csv", recursive=True)
+        csv_files = glob(root_path+"/**/metadata.csv", recursive=True)
     else:
         csv_files = meta_files
     # meta_files = [f.strip() for f in meta_files.split(",")]
@@ -140,15 +131,12 @@ def mailabs(root_path, meta_files=None):
                 if meta_files is None:
                     wav_file = os.path.join(folder, 'wavs', cols[0] + '.wav')
                 else:
-                    wav_file = os.path.join(root_path,
-                                            folder.replace("metadata.csv", ""),
-                                            'wavs', cols[0] + '.wav')
+                    wav_file = os.path.join(root_path, folder.replace("metadata.csv", ""), 'wavs', cols[0] + '.wav')
                 if os.path.isfile(wav_file):
                     text = cols[1].strip()
                     items.append([text, wav_file, speaker_name])
                 else:
-                    raise RuntimeError("> File %s does not exist!" %
-                                       (wav_file))
+                    raise RuntimeError("> File %s is not exist!"%(wav_file))
     return items
 
 
@@ -191,8 +179,8 @@ def common_voice(root_path, meta_file):
             cols = line.split("\t")
             text = cols[2]
             speaker_name = cols[0]
-            wav_file = os.path.join(root_path, "clips", cols[1].replace(".mp3", ".wav"))
-            items.append([text, wav_file, 'MCV_' + speaker_name])
+            wav_file = os.path.join(root_path, "clips", cols[1] + ".wav")
+            items.append([text, wav_file, speaker_name])
     return items
 
 
@@ -211,10 +199,9 @@ def libri_tts(root_path, meta_files=None):
                 cols = line.split('\t')
                 wav_file = os.path.join(_root_path, cols[0] + '.wav')
                 text = cols[1]
-                items.append([text, wav_file, 'LTTS_' + speaker_name])
+                items.append([text, wav_file, speaker_name])
     for item in items:
-        assert os.path.exists(
-            item[1]), f" [!] wav files don't exist - {item[1]}"
+        assert os.path.exists(item[1]), f" [!] wav file is not exist - {item[1]}"
     return items
 
 
@@ -226,115 +213,11 @@ def custom_turkish(root_path, meta_file):
     with open(txt_file, 'r', encoding='utf-8') as ttf:
         for line in ttf:
             cols = line.split('|')
-            wav_file = os.path.join(root_path, 'wavs',
-                                    cols[0].strip() + '.wav')
+            wav_file = os.path.join(root_path, 'wavs', cols[0].strip() + '.wav')
             if not os.path.exists(wav_file):
                 skipped_files.append(wav_file)
                 continue
             text = cols[1].strip()
             items.append([text, wav_file, speaker_name])
-    print(f" [!] {len(skipped_files)} files skipped. They don't exist...")
+    print(f" [!] {len(skipped_files)} files skipped. They are not exist...")
     return items
-
-
-# ToDo: add the dataset link when the dataset is released publicly
-def brspeech(root_path, meta_file):
-    '''BRSpeech 3.0 beta'''
-    txt_file = os.path.join(root_path, meta_file)
-    items = []
-    with open(txt_file, 'r') as ttf:
-        for line in ttf:
-            if line.startswith("wav_filename"):
-                continue
-            cols = line.split('|')
-            wav_file = os.path.join(root_path, cols[0])
-            text = cols[2]
-            speaker_name = cols[3]
-            items.append([text, wav_file, speaker_name])
-    return items
-
-
-def vctk(root_path, meta_files=None, wavs_path='wav48'):
-    """homepages.inf.ed.ac.uk/jyamagis/release/VCTK-Corpus.tar.gz"""
-    test_speakers = meta_files
-    items = []
-    meta_files = glob(f"{os.path.join(root_path,'txt')}/**/*.txt", recursive=True)
-    for meta_file in meta_files:
-        _, speaker_id, txt_file = os.path.relpath(meta_file,
-                                                  root_path).split(os.sep)
-        file_id = txt_file.split('.')[0]
-        if isinstance(test_speakers,
-                      list):  # if is list ignore this speakers ids
-            if speaker_id in test_speakers:
-                continue
-        with open(meta_file) as file_text:
-            text = file_text.readlines()[0]
-        wav_file = os.path.join(root_path, wavs_path, speaker_id,
-                                file_id + '.wav')
-        items.append([text, wav_file, 'VCTK_' + speaker_id])
-
-    return items
-
-
-def vctk_slim(root_path, meta_files=None, wavs_path='wav48'):
-    """homepages.inf.ed.ac.uk/jyamagis/release/VCTK-Corpus.tar.gz"""
-    items = []
-    txt_files = glob(f"{os.path.join(root_path,'txt')}/**/*.txt", recursive=True)
-    for text_file in txt_files:
-        _, speaker_id, txt_file = os.path.relpath(text_file,
-                                                  root_path).split(os.sep)
-        file_id = txt_file.split('.')[0]
-        if isinstance(meta_files, list):  # if is list ignore this speakers ids
-            if speaker_id in meta_files:
-                continue
-        wav_file = os.path.join(root_path, wavs_path, speaker_id,
-                                file_id + '.wav')
-        items.append([None, wav_file, 'VCTK_' + speaker_id])
-
-    return items
-
-# ======================================== VOX CELEB ===========================================
-def voxceleb2(root_path, meta_file=None):
-    """
-    :param meta_file   Used only for consistency with load_meta_data api
-    """
-    return _voxcel_x(root_path, meta_file, voxcel_idx="2")
-
-
-def voxceleb1(root_path, meta_file=None):
-    """
-    :param meta_file   Used only for consistency with load_meta_data api
-    """
-    return _voxcel_x(root_path, meta_file, voxcel_idx="1")
-
-
-def _voxcel_x(root_path, meta_file, voxcel_idx):
-    assert voxcel_idx in ["1", "2"]
-    expected_count = 148_000 if voxcel_idx == "1" else 1_000_000
-    voxceleb_path = Path(root_path)
-    cache_to = voxceleb_path / f"metafile_voxceleb{voxcel_idx}.csv"
-    cache_to.parent.mkdir(exist_ok=True)
-
-    # if not exists meta file, crawl recursively for 'wav' files
-    if meta_file is not None:
-        with open(str(meta_file), 'r') as f:
-            return [x.strip().split('|') for x in f.readlines()]
-
-    elif not cache_to.exists():
-        cnt = 0
-        meta_data = []
-        wav_files = voxceleb_path.rglob("**/*.wav")
-        for path in tqdm(wav_files, desc=f"Building VoxCeleb {voxcel_idx} Meta file ... this needs to be done only once.",
-                         total=expected_count):
-            speaker_id = str(Path(path).parent.parent.stem)
-            assert speaker_id.startswith('id')
-            text = None  # VoxCel does not provide transciptions, and they are not needed for training the SE
-            meta_data.append(f"{text}|{path}|voxcel{voxcel_idx}_{speaker_id}\n")
-            cnt += 1
-        with open(str(cache_to), 'w') as f:
-            f.write("".join(meta_data))
-        if cnt < expected_count:
-            raise ValueError(f"Found too few instances for Voxceleb. Should be around {expected_count}, is: {cnt}")
-
-    with open(str(cache_to), 'r') as f:
-        return [x.strip().split('|') for x in f.readlines()]
